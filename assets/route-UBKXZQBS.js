8291c917859f10b3d94c6f23629e90ebd05a9d0d
Miabread <miabreadloaf@gmail.com>
2025-03-09 21:26:26 -0400

    Updates

---
+++ assets/route-UBKXZQBS.js
@@ -0,0 +1,6 @@
+import{c as o,j as s,H as n,L as r,S as c,O as l}from"./index-DCaobMem.js";import{c as t}from"./index-8T8DQm4w.js";/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var i=o("outline","briefcase","IconBriefcase",[["path",{d:"M3 7m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z",key:"svg-0"}],["path",{d:"M8 7v-2a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v2",key:"svg-1"}],["path",{d:"M12 12l0 .01",key:"svg-2"}],["path",{d:"M3 13a20 20 0 0 0 18 0",key:"svg-3"}]]);const x=function(){return s.jsxs(s.Fragment,{children:[s.jsxs("aside",{className:"bg-section w-4xl mr-auto m-10 rounded-lg p-10 inset-shadow-xl",children:[s.jsxs(n,{color:"eleri",children:[s.jsx(i,{})," Classes"]}),s.jsx("nav",{className:"flex flex-col",children:Object.entries(t).map(([a,e])=>s.jsxs(r,{to:"/classes/$classId",params:{classId:a},children:[e.name," ",s.jsx(c,{source:e.source,page:e.page})]}))})]}),s.jsx("div",{className:"bg-section w-full m-10 rounded-lg p-10 overflow-y-auto inset-shadow-xl",children:s.jsx(l,{})})]})};export{x as component};
