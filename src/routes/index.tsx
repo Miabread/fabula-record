197c09095580fa29ca89ee7f7d0a026f72429445
Miabread <miabreadloaf@gmail.com>
2025-03-09 21:58:48 -0400

    Remove dead links for now

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,5 @@
 import { createFileRoute, Link } from '@tanstack/react-router';
-import {
-    IconBallpen,
-    IconBriefcase2,
-    IconChartPie,
-    IconComet,
-    IconFileSearch,
-    IconLibrary,
-    IconMoon,
-    IconSparkles,
-    IconSun,
-    IconSwords,
-    IconUser,
-} from '@tabler/icons-react';
+import { IconBriefcase2, IconComet, IconMoon, IconSun } from '@tabler/icons-react';
 import { Heading, ButtonRow, Button } from '../components';
 
 export const Route = createFileRoute('/')({
@@ -22,51 +10,23 @@ function Index() {
     return (
         <div className="bg-section w-full my-10 mx-30 rounded-lg p-10 inset-shadow-xl">
             <Heading color="eleri">
-                <IconMoon /> Eleri's Guide of Distant Travels
+                <IconMoon /> Eleri's Stories of Far Travels
             </Heading>
             <ButtonRow>
                 <Link to="/classes">
                     <Button color="eleri">
                         <IconBriefcase2 /> Classes
-                    </Button>{' '}
+                    </Button>
                 </Link>
-                <Button color="eleri">
-                    <IconSparkles /> Heroic Skills
-                </Button>
             </ButtonRow>
 
             <Heading color="zatari">
                 <IconSun /> Zatari's Laws of Love and War
             </Heading>
-            <ButtonRow>
-                <Button color="zatari">
-                    <IconSwords /> Equipment
-                </Button>
-                <Button color="zatari">
-                    <IconUser /> NPC Skills
-                </Button>
-            </ButtonRow>
 
             <Heading color="fuji">
                 <IconComet /> Fuji's Account of Twisted Fates
             </Heading>
-            <ButtonRow>
-                <Button color="fuji">
-                    <IconBallpen /> Quirks
-                </Button>
-                <Button color="fuji">
-                    <IconChartPie /> Zero Powers
-                </Button>
-            </ButtonRow>
-
-            <Heading color="maria">
-                <IconLibrary /> Maria's Bundle of Research Notes
-            </Heading>
-            <ButtonRow>
-                <Button color="maria">
-                    <IconFileSearch /> Glossary
-                </Button>
-            </ButtonRow>
         </div>
     );
 }
